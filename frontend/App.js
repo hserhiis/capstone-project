da2cf4862b7c8858c5d070171ba171735c5f48ef
Serhii Shapovalov <hserhiis@Serhiis-MacBook-Air.local>
2023-10-28 17:11:11 -0400

    changed app.js file text

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fuck</Text>
+      <Text>Does not work</Text>
       <StatusBar style="auto" />
     </View>
   );
