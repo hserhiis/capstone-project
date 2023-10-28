eed25ea44df184b460d240fc461a3ed3f601b620
Serhii Shapovalov <hserhiis@Serhiis-MacBook-Air.local>
2023-10-28 17:15:06 -0400

    changed app.js file te

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Does not work</Text>
+      <Text>Does not wor</Text>
       <StatusBar style="auto" />
     </View>
   );
